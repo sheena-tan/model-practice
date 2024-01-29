0811ee48823fc33366749cf1d42f44be7f81ab79
akuyper <a-kuyper@northwestern.edu>
2024-01-29 04:14:08 -0600

    Setting up template scripts

--- exercise_1/3_lasso_training.R
+++ exercise_1/3_lasso_training.R
@@ -1,4 +1,33 @@
-# L03 Model workflows & recipes ----
-# WHAT DOES SCRIPT DO?
+# L04 Judging Models ----
+# Define and fit penalized regression (lasso)
 
 ## load packages ----
+library(tidyverse)
+library(tidymodels)
+library(here)
+
+# handle common conflicts
+tidymodels_prefer()
+
+# load training data
+kc_train <- read_rds(here("data/kc_train.rds"))
+
+# load preporcessing/feature engineering/recipe
+
+# model specifications
+lm_spec <- 
+  linear_reg() |> 
+  set_engine("lm") |> 
+  set_mode("regression") 
+
+# define workflows
+lm_wflow <- 
+  workflow() |> 
+  add_model(lm_spec) |> 
+  add_recipe(kc_recipe)
+
+# fit workflows/models
+fit_lm <- fit(lm_wflow, kc_train)
+
+# write out results (fitted/trained workflows)
+save(fit_lm, file = here("results/fit_lm.rda"))
--- exercise_1/3_rf_training.R
+++ exercise_1/3_rf_training.R
@@ -1,4 +1,34 @@
-# L03 Model workflows & recipes ----
-# WHAT DOES SCRIPT DO?
+# L04 Judging Models ----
+# Define and fit random forest model
 
 ## load packages ----
+library(tidyverse)
+library(tidymodels)
+library(here)
+
+# handle common conflicts
+tidymodels_prefer()
+
+# load training data
+
+
+
+# load preporcessing/feature engineering/recipe
+
+
+
+# model specifications
+# don't worry about hyperparameters (mtry and trees) -- we will cover later
+rf_spec <- 
+  rand_forest(mtry = 3, trees = 500) %>%
+  set_engine("ranger") %>% 
+  set_mode("regression")
+
+# define workflows
+
+
+# fit workflows/models
+
+
+# write out results (fitted/trained workflows)
+
